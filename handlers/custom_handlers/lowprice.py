7f1b7966e3ee764465c447347a0c5a12443447dd
Морозов Андрей <yerdnavozorom@gmail.com>
2022-08-13 21:36:08 +0300

    вынес функции по работе с api в отдельный модуль

--- handlers/custom_handlers/lowprice.py
+++ handlers/custom_handlers/lowprice.py
@@ -1,15 +1,14 @@
 from loader import bot
 from telebot.types import Message
 from config_data.config import LOW_PRICE_COMMAND
-from config_data.config import RAPID_API_KEY
-import requests
+from utils.misc.rapid_api_utils import get_lowprice_hotels
 import json
 
 
 @bot.message_handler(commands=[LOW_PRICE_COMMAND['command_name']])
 def lowprice(message: Message) -> None:
     bot.send_message(message.from_user.id, 'вы выбрали показать топ самых дешевых отелей в городе. Введите город')
-    is_success, hotels = get_hotels('new york')
+    is_success, hotels = get_lowprice_hotels('new york')
     if is_success:
         with open('result.json', 'w') as file:
             json.dump(hotels, file, indent=4)
@@ -22,85 +21,3 @@ def lowprice_button_message(message: Message) -> None:
         lowprice(message)
 
 
-def get_hotels(town):
-    is_finded, locations = get_locations_from_server('new york')
-
-    if is_finded:
-        town_id = locations[0][0]
-        is_finded, hotels = get_hotels_from_server(town_id)
-
-        return is_finded, hotels
-
-    return False, None
-
-
-def get_hotels_from_server(town_id: str):
-    """
-    получает отели в выбранной локации
-    """
-    url = 'https://hotels4.p.rapidapi.com/properties/list'
-    params = {
-        'destinationId': town_id,
-        'pageNumber': '1',
-        'pageSize': '10',
-        'checkIn': '2022-08-13',
-        'checkOut': '2022-08-15',
-        'adults1': '1',
-        'sortOrder': 'PRICE',
-        'locale': 'en_US',
-        'currency': 'USD'
-    }
-    headers = {
-        'X-RapidAPI-Key': RAPID_API_KEY,
-        'X-RapidAPI-Host': 'hotels4.p.rapidapi.com'
-    }
-    answer = requests.get(url, params=params, headers=headers)
-    hotels_info = json.loads(answer.text)
-
-    data = hotels_info.get('data', None)
-    if data is None:
-        return False, None
-
-    body = data.get('body', None)
-    if body is None:
-        return False, None
-
-    search_results = body.get('searchResults', None)
-    if search_results is None:
-        return False, None
-
-    hotels = search_results.get('results', None)
-    return True, hotels
-
-
-
-def get_locations_from_server(town: str)  -> (bool, [(str, str)]):
-    """
-    получает локации от сервера, локациями считаются только города
-    """
-    url = 'https://hotels4.p.rapidapi.com/locations/v2/search'
-    params = {
-        'query': town,
-        'locale': 'en_US',
-        'currency': 'USD'
-    }
-    headers = {
-        'X-RapidAPI-Key': RAPID_API_KEY,
-        'X-RapidAPI-Host': 'hotels4.p.rapidapi.com'
-    }
-    answer = requests.get(url, params=params, headers=headers)
-    locations = json.loads(answer.text)
-
-    if len(locations) > 0:
-        group_finded = False
-        for location in locations.get("suggestions"):
-            group = location.get("group", None)
-            if group == "CITY_GROUP":
-                group_finded = True
-                break
-
-        if group_finded:
-            places = location.get('entities', None)
-            if places is not None:
-                return True, [(place.get('destinationId'), place.get('name')) for place in places if place.get("type", "") == "CITY"]
-    return False, None
---
+++ utils/misc/rapid_api_utils.py
@@ -0,0 +1,89 @@
+import requests
+import json
+from config_data.config import RAPID_API_KEY
+
+def get_lowprice_hotels(town):
+    return get_hotels(town, '2022-08-15', '2022-08-16', 'PRICE')
+
+def get_hotels(town: str, date_in: str, date_out: str, sort_rule: str):
+    is_finded, locations = get_locations_from_server(town)
+
+    if is_finded:
+        town_id = locations[0][0]
+        is_finded, hotels = get_hotels_from_server(town_id, date_in, date_out, sort_rule)
+
+        return is_finded, hotels
+
+    return False, None
+
+
+def get_hotels_from_server(town_id: str, date_in: str, date_out: str, sort_rule: str):
+    """
+    получает отели в выбранной локации
+    """
+    url = 'https://hotels4.p.rapidapi.com/properties/list'
+    params = {
+        'destinationId': town_id,
+        'pageNumber': '1',
+        'pageSize': '10',
+        'checkIn': date_in,
+        'checkOut': date_out,
+        'adults1': '1',
+        'sortOrder': sort_rule,
+        'locale': 'en_US',
+        'currency': 'USD'
+    }
+    headers = {
+        'X-RapidAPI-Key': RAPID_API_KEY,
+        'X-RapidAPI-Host': 'hotels4.p.rapidapi.com'
+    }
+    answer = requests.get(url, params=params, headers=headers)
+    hotels_info = json.loads(answer.text)
+
+    data = hotels_info.get('data', None)
+    if data is None:
+        return False, None
+
+    body = data.get('body', None)
+    if body is None:
+        return False, None
+
+    search_results = body.get('searchResults', None)
+    if search_results is None:
+        return False, None
+
+    hotels = search_results.get('results', None)
+    return True, hotels
+
+
+
+def get_locations_from_server(town: str)  -> (bool, [(str, str)]):
+    """
+    получает локации от сервера, локациями считаются только города
+    """
+    url = 'https://hotels4.p.rapidapi.com/locations/v2/search'
+    params = {
+        'query': town,
+        'locale': 'en_US',
+        'currency': 'USD'
+    }
+    headers = {
+        'X-RapidAPI-Key': RAPID_API_KEY,
+        'X-RapidAPI-Host': 'hotels4.p.rapidapi.com'
+    }
+    answer = requests.get(url, params=params, headers=headers)
+    locations = json.loads(answer.text)
+
+    if len(locations) > 0:
+        group_finded = False
+        for location in locations.get("suggestions"):
+            group = location.get("group", None)
+            if group == "CITY_GROUP":
+                group_finded = True
+                break
+
+        if group_finded:
+            places = location.get('entities', None)
+            if places is not None:
+                return True, [(place.get('destinationId'), place.get('name')) for place in places if place.get("type", "") == "CITY"]
+    return False, None
