d1247b0e4ce6a623f5764f2c5d89865d00ddf928
Морозов Андрей <yerdnavozorom@gmail.com>
2022-08-15 21:08:41 +0300

    Выделил взаимодействие с сервером в отдельную функцию

--- utils/misc/rapid_api_utils.py
+++ utils/misc/rapid_api_utils.py
@@ -2,8 +2,22 @@ import requests
 import json
 from config_data.config import RAPID_API_KEY
 
+
+def send_data_to_server(url, params, headers):
+    try:
+        response = requests.get(url, headers=headers, params=params, timeout=10)
+        if response.status_code == requests.codes.ok:
+            return response
+        print('Ошибка', response.status_code)
+        return None
+    except requests.ConnectionError:
+        print('Ошибка соединения')
+        return None
+
+
 def get_lowprice_hotels(town):
-    return get_hotels(town, '2022-08-15', '2022-08-16', 'PRICE')
+    return get_hotels(town, '2022-08-20', '2022-08-21', 'PRICE')
+
 
 def get_hotels(town: str, date_in: str, date_out: str, sort_rule: str):
     is_finded, locations = get_locations_from_server(town)
@@ -37,27 +51,28 @@ def get_hotels_from_server(town_id: str, date_in: str, date_out: str, sort_rule:
         'X-RapidAPI-Key': RAPID_API_KEY,
         'X-RapidAPI-Host': 'hotels4.p.rapidapi.com'
     }
-    answer = requests.get(url, params=params, headers=headers)
-    hotels_info = json.loads(answer.text)
+    answer = send_data_to_server(url, params, headers)
+    if answer is not None:
+        hotels_info = json.loads(answer.text)
 
-    data = hotels_info.get('data', None)
-    if data is None:
-        return False, None
+        data = hotels_info.get('data', None)
+        if data is None:
+            return False, None
 
-    body = data.get('body', None)
-    if body is None:
-        return False, None
+        body = data.get('body', None)
+        if body is None:
+            return False, None
 
-    search_results = body.get('searchResults', None)
-    if search_results is None:
-        return False, None
+        search_results = body.get('searchResults', None)
+        if search_results is None:
+            return False, None
 
-    hotels = search_results.get('results', None)
-    return True, hotels
+        hotels = search_results.get('results', None)
+        return True, hotels
+    return False, None
 
 
-
-def get_locations_from_server(town: str)  -> (bool, [(str, str)]):
+def get_locations_from_server(town: str) -> (bool, [(str, str), (str, str), ...]):
     """
     получает локации от сервера, локациями считаются только города
     """
@@ -71,19 +86,20 @@ def get_locations_from_server(town: str)  -> (bool, [(str, str)]):
         'X-RapidAPI-Key': RAPID_API_KEY,
         'X-RapidAPI-Host': 'hotels4.p.rapidapi.com'
     }
-    answer = requests.get(url, params=params, headers=headers)
-    locations = json.loads(answer.text)
+    answer = send_data_to_server(url, params, headers)
+    if answer is not None:
+        locations = json.loads(answer.text)
 
-    if len(locations) > 0:
-        group_finded = False
-        for location in locations.get("suggestions"):
-            group = location.get("group", None)
-            if group == "CITY_GROUP":
-                group_finded = True
-                break
+        if len(locations) > 0:
+            group_finded = False
+            for location in locations.get("suggestions"):
+                group = location.get("group", None)
+                if group == "CITY_GROUP":
+                    group_finded = True
+                    break
 
-        if group_finded:
-            places = location.get('entities', None)
-            if places is not None:
-                return True, [(place.get('destinationId'), place.get('name')) for place in places if place.get("type", "") == "CITY"]
+            if group_finded:
+                places = location.get('entities', None)
+                if places is not None:
+                    return True, [(place.get('destinationId'), place.get('name')) for place in places if place.get("type", "") == "CITY"]
     return False, None
