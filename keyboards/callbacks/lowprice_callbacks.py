60e049b3dbc8df0480c586e48208877655a9cdcd
Андрей Морозов <yerdnavozorom@gmail.com>
2022-08-17 15:53:49 +0300

    Добавил обновление информации по отелю в соощении без удаления

--- keyboards/callbacks/lowprice_callbacks.py
+++ keyboards/callbacks/lowprice_callbacks.py
@@ -4,13 +4,17 @@ from db.hotels_parser import get_lowprice_data_from_server
 from db.hotels_parser import get_lowprice_hotel_data
 from handlers.custom_handlers.lowprice import lowprice_data
 
+
 @bot.callback_query_handler(func=lambda call: call.data.split('#')[0]=='lowprice_page_numbers')
 def hotels_page_callback(call):
     pages_cnt = int(call.data.split('#')[1])
     print('here')
+
+    bot.delete_message(chat_id=call.message.chat.id, message_id=call.message.message_id)
+
     bot.send_message(chat_id=call.message.chat.id, text='Все данные получены, начинаю загрузку отелей')
 
-    get_lowprice_data_from_server()
+    #get_lowprice_data_from_server()
     #with bot.retrieve_data(call.message.from_user.id, call.message.chat.id) as data:
     lowprice_data[call.message.chat.id].hotels_number = pages_cnt
     bot.delete_state(call.message.from_user.id, call.message.chat.id)
@@ -26,20 +30,10 @@ def hotels_page_callback(call):
     )
 
 
-
 @bot.callback_query_handler(func=lambda call: call.data.split('#')[0]=='lowprice_page')
 def hotel_page_callback(call):
     page = int(call.data.split('#')[1])
-    bot.delete_message(
-        call.message.chat.id,
-        call.message.message_id
-    )
     pages_cnt = lowprice_data[call.message.chat.id].hotels_number
     paginator = get_lowprice_paginator(page, pages_cnt)
     hotel_data = get_lowprice_hotel_data(page - 1)
-    bot.send_message(
-        call.message.chat.id,
-        hotel_data,
-        reply_markup=paginator.markup,
-        parse_mode='Markdown'
-    )
+    bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text=hotel_data, reply_markup=paginator.markup)
